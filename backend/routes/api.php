323fac75be6e32f47a5c85c722294a8233b35643
aynoncse <aynoncse@gmail.com>
2026-03-18 17:49:08 +0600

    fix(backend): make password column nullable

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Models\User;
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware('firebase.auth')->group(function () {
     Route::get('/users', function (Request $request) {
         return User::all();
     });
