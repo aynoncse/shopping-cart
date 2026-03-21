eef0fd9e0c629227793e656e972a3a76f8ac788d
aynoncse <aynoncse@gmail.com>
2026-03-21 12:31:07 +0600

    refactor(backend): remove unncessary image_url attribute

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
@@ -10,20 +9,11 @@ class Product extends Model
     protected $fillable = ['name', 'description', 'price', 'image'];
 
     protected $casts = [
-        'price' => 'double'
+        'price' => 'double',
     ];
-
-    protected $appends = ['image_url'];
 
     public function cartItems()
     {
         return $this->hasMany(Cart::class);
-    }
-
-    public function imageUrl(): Attribute
-    {
-        return Attribute::make(
-            get: fn () => asset($this->image),
-        );
     }
 }
