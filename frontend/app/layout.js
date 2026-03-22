3898f848de7f8bb06f22e2d12c64ca0bc253c980
aynoncse <aynoncse@gmail.com>
2026-03-22 18:47:07 +0600

    chore: update favicon

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -17,7 +17,9 @@ export default function RootLayout({ children }) {
         <div className="min-h-screen flex flex-col">
           <Providers>
             <Navbar />
-            <main className="mx-auto w-full max-w-7xl grow px-3 sm:px-4 lg:px-6">{children}</main>
+            <main className="mx-auto w-full max-w-7xl grow px-3 sm:px-4 lg:px-6">
+              {children}
+            </main>
             <Footer />
             <CartDrawer />
             <ToasterProvider />
